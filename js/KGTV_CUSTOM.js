4d08dfe6ad0e164144f8d72102f989666961217f
AndrewMoore10 <Andrew.Moore@10news.com>
2017-01-06 14:48:17 -0800

    added custom kgtv.css
    added css to hide password change

--- js/KGTV_CUSTOM.js
+++
@@ -1,6 +0,0 @@
-$('body').on('DOMNodeInserted',function(e){
-  var element = e.target; //inserted element;
-  console.log("ready; hiding password button");
-  element.hide();
-  console.log(element);
-});
